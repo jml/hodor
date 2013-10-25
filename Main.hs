26005a668727f4ba11f288309ffa90bc544425be
Jonathan Lange <jml@mumak.net>
2013-10-25 22:54:47 +0100

    Lots of changes from IRC

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
+-- Scope?
 import Data.Either
 import Data.Maybe
+-- Is this the modern way to import it?
 import Text.ParserCombinators.Parsec
 
 
@@ -9,9 +11,8 @@ data Project = Project String deriving Show
 data Context = Context String deriving Show
 
 
-
 data TodoItem = TodoItem {
-  dateCompleted :: Maybe Date,
+  completed :: Maybe Date,
   priority :: Maybe Char,
   dateCreated :: Maybe Date,
   projects :: [Project],
@@ -19,34 +20,38 @@ data TodoItem = TodoItem {
 } deriving Show
 
 
+-- XXX: Unit tests!
+todoSample :: String
 todoSample = unlines [
   "(B) 2013-09-27 Wipe mould off bathroom ceiling +condensation @home",
-  "2013-09-21 Email Will arranging time to catch up @online +leading-kg",
-  "x 2013-10-12 2013-09-20 Read Acts +leading-kg",
+  "2013-09-21 Email John arranging time to catch up @online +some-project",
+  "x 2013-10-12 2013-09-20 A done task +some-project",
   "x 2013-10-12 Something else"
   ]
 
 
-todoTxtFile :: GenParser Char st [TodoItem]
+todoTxtFile :: Parser [TodoItem]
 todoTxtFile = endBy line eol
 
-eol :: GenParser Char st Char
+eol :: Parser Char
 eol = char '\n'
 
-line :: GenParser Char st TodoItem
+line :: Parser TodoItem
 line = do
   dateCompleted <- optionMaybe completion
   p <- optionMaybe priorityField
   created <- optionMaybe date
   words <- sepBy word (char ' ')
   let (ps, cs) = partitionEithers $ catMaybes words
-    in return (TodoItem dateCompleted p created ps cs)
+  return (TodoItem dateCompleted p created ps cs)
 
 
 completion = char 'x' >> char ' ' >> date
 
-word :: GenParser Char st (Maybe (Either Project Context))
-word = (try project >>= return . Just . Left)
+-- XXX: This is pretty hideous, I wonder if there's a better way.
+-- Suggestion: a rest-of-line: Parser [Either Project Context]
+word :: Parser (Maybe (Either Project Context))
+word =     (try project >>= return . Just . Left)
        <|> (try context >>= return . Just . Right)
        <|> (bareword >> return Nothing)
 
@@ -72,6 +77,7 @@ priorityField =
     return p
 
 
+-- XXX: I bet this can be expressed more succinctly
 date = do
   year <- count 4 digit
   char '-'
@@ -79,13 +85,24 @@ date = do
   char '-'
   day <- count 2 digit
   char ' '
+  -- XXX: Figure out the actual date type and return that.
   return (year, month, day)
 
 
+-- XXX: Possibly replace (parseTodoText todoSample) in main by inlining its
+-- definition and supplying `parse` with the filename, so that it comes out as
+--   case parse todoTxtFile filename file_contents of -> ...
+
+-- XXX: Don't just want to parse metadata, but also need to associate with
+-- line number and with full text.  something like:
+--   (i, line, parsed(line)) for i, line in enumerate(lines)
 parseTodoTxt :: String -> Either ParseError [TodoItem]
 parseTodoTxt input = parse todoTxtFile "(unknown)" input
 
 
+-- XXX: How do you do command-line arguments?
+-- System.Console.GetOpt
+main :: IO ()
 main = do
   case parseTodoTxt todoSample of
     Left e -> do putStrLn "Error parsing input"
