e2e273db3c5c57deeb8315abac1fb0a7602cd052
Jonathan Lange <jml@mumak.net>
2013-11-19 20:07:19 +0000

    project review

--- Main.hs
+++ Main.hs
@@ -1,7 +1,16 @@
+import Control.Monad.Trans.Error (ErrorT(ErrorT), runErrorT, Error, strMsg)
+import Data.Either (lefts)
+import Data.Maybe (maybe)
 import Data.List (intercalate)
+import Data.Time
 import System.IO
+import Text.ParserCombinators.Parsec (ParseError, parse)
 
-import Hodor (parseTodoFile, groupByProjects, items, description, Project, TodoItem, TodoFile)
+import Hodor (dateCompleted, parseTodoFile, groupByProjects, items, description, Project, TodoItem, TodoFile)
+
+
+instance Error ParseError where
+    strMsg s = head (lefts [(parse (fail s) "" [])])
 
 
 renderProjectAndItems :: (Project, [TodoItem]) -> String
@@ -9,20 +18,30 @@ renderProjectAndItems (p, ts) =
   show p ++ "\n" ++ intercalate "\n" (map (("  "++) . description) ts) ++ "\n"
 
 
-projectReview :: TodoFile -> String
-projectReview = concat . map renderProjectAndItems . groupByProjects . items
+projectReview :: [TodoItem] -> String
+projectReview = concat . map renderProjectAndItems . groupByProjects
 
 
 
-
+parseFile :: String -> IO (Either ParseError TodoFile)
 parseFile filename = do
   contents <- readFile filename
-  putStrLn $ case parseTodoFile filename contents of
-    Left error -> show error
-    Right todoFile -> projectReview todoFile
+  return $ parseTodoFile filename contents
+
+
+parseFiles :: [String] -> IO (Either ParseError [TodoFile])
+parseFiles files = runErrorT $ sequence (map ErrorT (map parseFile files))
+
+closedSince :: Day -> TodoItem -> Bool
+closedSince day = maybe True (>= day) . dateCompleted
 
 
 -- XXX: How do you do command-line arguments?
 -- System.Console.GetOpt
 main :: IO ()
-main = parseFile "/home/jml/.todo/todo.txt"
+main = do
+  today <- getCurrentTime >>= return . utctDay
+  result <- parseFiles ["/home/jml/.todo/todo.txt", "/home/jml/.todo/done.txt"]
+  putStrLn $ case result of
+    Left error -> show error
+    Right todoFiles -> projectReview $ filter (closedSince $ addDays (-7) today) $ concatMap items todoFiles
