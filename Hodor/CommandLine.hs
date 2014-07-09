ba674a9867897ac1aa29127f24605c28a7a0a26b
Jonathan Lange <jml@mumak.net>
2014-07-09 23:15:30 +0100

    Basic command support

--- Hodor/CommandLine.hs
+++ Hodor/CommandLine.hs
@@ -1,6 +1,7 @@
 module Hodor.CommandLine where
 
 import Data.List ( intercalate )
+import qualified Data.Map as M
 import Data.Maybe ( fromMaybe )
 import System.Console.GetOpt
 import System.Environment (getArgs)
@@ -54,9 +55,9 @@ hodorOpts argv =
   where header = "Usage: hodor [OPTION...] "
 
 
-failIfLeft :: Either IOError b -> IO b
-failIfLeft (Left e) = ioError e
-failIfLeft (Right result) = return result
+usageError :: [String] -> IOError
+usageError errs = userError (concat errs ++ usageInfo header options)
+                  where header = "Usage: hodor [OPTION...] "
 
 
 -- XXX: Is there a better way of doing this?
@@ -72,9 +73,36 @@ getConfiguration ((DoneFile path):xs) = do
 getConfiguration [] = return defaultConfig
 
 
-todoList :: TodoFile -> [String] -> IO ()
-todoList todoFile _ = do
+readTodoFileEx :: FilePath -> IO TodoFile
+readTodoFileEx path = do
+  result <- readTodoFile path
+  case result of
+    Left e -> ioError $ userError $ show e
+    Right r -> return r
+
+
+type HodorCommand = Config -> [String] -> IO ()
+
+cmdList :: HodorCommand
+cmdList config _ = do
+  todoFile <- readTodoFileEx (todoFilePath config)
   putStrLn $ intercalate "\n" $ map unparse (todoFileItems todoFile)
+
+
+commands :: M.Map String HodorCommand
+commands = M.fromList [
+  ("list", cmdList),
+  ("ls", cmdList)
+  ]
+
+
+getHodorCommand :: Config -> [String] -> IO ()
+getHodorCommand config [] = cmdList config []
+getHodorCommand config (cmd:args) =
+  case M.lookup cmd commands of
+    Just command -> command config args
+    Nothing -> ioError $ usageError (["No such command: ", cmd, "\n"])
+
 
 
 main :: IO ()
@@ -84,8 +112,4 @@ main = do
     Left e -> ioError e
     Right result -> return result
   config <- getConfiguration opts
-  result <- readTodoFile (todoFilePath config)
-  todoFile <- case result of
-    Left e -> ioError $ userError $ show e
-    Right r -> return r
-  todoList todoFile args
+  getHodorCommand config args
