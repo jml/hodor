f44a6f39d270d4bad182070bd9b36e4dfce572dd
Jonathan Lange <jml@mumak.net>
2014-07-25 12:23:37 +0200

    File for formatting tests

--- Tests.hs
+++ Tests.hs
@@ -2,12 +2,14 @@ module Main where
 
 import Test.Hspec
 
+import qualified Tests.FormatSpec
 import qualified Tests.ParserSpec
 import qualified Tests.TypesSpec
 
 
 spec :: Spec
 spec = do
+  Tests.FormatSpec.spec
   Tests.ParserSpec.spec
   Tests.TypesSpec.spec
 
---
+++ Tests/FormatSpec.hs
@@ -0,0 +1,8 @@
+module Tests.FormatSpec (spec) where
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Formatting events" $
+    it "works" pending
