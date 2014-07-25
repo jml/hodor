67355c4ed67fda7c8c0e18abb28706b240d87150
Jonathan Lange <jml@mumak.net>
2014-07-25 21:50:03 +0200

    Write ordering tests as quickcheck

--- Tests/TypesSpec.hs
+++ Tests/TypesSpec.hs
@@ -2,20 +2,24 @@ module Tests.TypesSpec (spec) where
 
 import Data.Time (fromGregorian)
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
 
 import Hodor.Parser (parseTodoFile)
 import Hodor.Types (
   dateCompleted,
   doItems,
   listItems,
-  unsafeMakePriority,
   makeTodoFile,
   noPriority,
+  Priority(Pri),
   TaskAction(..),
   TodoEvent(..),
   TodoFile,
   unsafeGetItem
   )
+
+import Tests.Generators
 
 
 emptyFile :: TodoFile
@@ -58,13 +62,12 @@ spec = describe "Core operations on todos" $ do
         unsafeGetItem (fst todoWriter) index `shouldBe` originalItem { dateCompleted = Just someDay }
 
   describe "priorities" $ do
-    describe "ordering" $ do
-      it "ranks A above B" $ do
-        unsafeMakePriority 'A' < unsafeMakePriority 'B' `shouldBe` True
-        unsafeMakePriority 'A' >= unsafeMakePriority 'B' `shouldBe` False
-      it "ranks A above D" $ do
-        unsafeMakePriority 'A' < unsafeMakePriority 'D' `shouldBe` True
-        unsafeMakePriority 'A' >= unsafeMakePriority 'D' `shouldBe` False
-      it "ranks any priority above none" $ do
-        unsafeMakePriority 'A' < noPriority `shouldBe` True
-        unsafeMakePriority 'Z' < noPriority `shouldBe` True
+    describe "x < y means x has higher priority than y" $ do
+      prop "earlier characters have higher priority (A) < (Z)" $
+        forAll arbitraryPriority $ \(Pri a) ->
+        forAll arbitraryPriority $ \(Pri b) -> a < b ==> Pri a < Pri b
+      prop "later characters have lower priority (Z) > (A)" $
+        forAll arbitraryPriority $ \(Pri a) ->
+        forAll arbitraryPriority $ \(Pri b) -> a > b ==> Pri a > Pri b
+      prop "no priority is the lowest priority" $
+        forAll arbitraryPriority $ \p -> p < noPriority
