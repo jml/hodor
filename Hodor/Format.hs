5c0f57ea99c1ce970ee0e6e4232ae41240ea4b0c
Jonathan Lange <jml@mumak.net>
2014-07-25 10:42:13 +0200

    Give main events ability to format based on change

--- Hodor/Format.hs
+++ Hodor/Format.hs
@@ -32,12 +32,25 @@ formatTodo i t = printf "%02d %s" i (unparse t)
 
 
 formatEvent' :: TaskAction -> Int -> TodoItem -> TodoItem -> String
-formatEvent' Done i _ _ = printf "%d marked as done." i
+formatEvent' Done i o _ | isDone o  = printf "%d is already marked done." i
+                        | otherwise = printf "%d marked as done." i
 formatEvent' AlreadyDone i _ _ = printf "%d is already marked done." i
-formatEvent' Undone i _ _ = printf "%d no longer marked as done." i
+
+formatEvent' Undone i o _ | isDone o =  printf "%d no longer marked as done." i
+                          | otherwise = printf "%d was already not marked done." i
 formatEvent' AlreadyNotDone i _ _ = printf "%d was already not marked done." i
-formatEvent' Prioritized i t _ = printf "%d prioritized %s." i (format (priority t))
-formatEvent' AlreadyPrioritized i t _ = printf "%d already prioritized %s." i (format (priority t))
-formatEvent' (ChangedPriority p) i t _ = printf "%d re-prioritized from %s to %s." i (format p) (format (priority t))
-formatEvent' Deprioritized i _ _ = printf "%d deprioritized." i
+
+formatEvent' Prioritized i o t =
+  if hasPriority o
+  then if oldPriority == newPriority
+       then printf "%d already prioritized %s." i (format oldPriority)
+       else printf "%d re-prioritized from %s to %s." i (format oldPriority) (format newPriority)
+  else printf "%d prioritized %s." i (format oldPriority)
+  where oldPriority = priority o
+        newPriority = priority t
+formatEvent' AlreadyPrioritized i o _ = printf "%d already prioritized %s." i (format (priority o))
+formatEvent' (ChangedPriority p) i o _ = printf "%d re-prioritized from %s to %s." i (format p) (format (priority o))
+
+formatEvent' Deprioritized i o _ | hasPriority o = printf "%d deprioritized." i
+                                 | otherwise     = printf "%d is not prioritized." i
 formatEvent' AlreadyDeprioritized i _ _ = printf "%d is not prioritized." i
