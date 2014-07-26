763999ac68090b055b4c8fd17e0227c6d453264c
Jonathan Lange <jml@mumak.net>
2014-07-26 20:43:07 +0200

    `shouldBe` returns better errors

--- Tests/TypesSpec.hs
+++ Tests/TypesSpec.hs
@@ -60,19 +60,19 @@ actionsSpec = describe "High-level operations on todos" $ do
     describe "for valid items" $ do
       prop "marks the item as done" $
         \day -> forAll todoFileIndexes $
-        \(file, index) -> getItem (fst $ doItems day file [index]) index ==
+        \(file, index) -> getItem (fst $ doItems day file [index]) index `shouldBe`
                           fmap (flip markAsDone day) (getItem file index)
       prop "reports that it marks item as done" $
         \day -> forAll todoFileIndexes $
         \(file, index) ->
         let (newTodoFile, events) = doItems day file [index] in
-        events == [TaskChanged Done index (unsafeGetItem file index)
-                   (unsafeGetItem newTodoFile index)]
+        events `shouldBe` [TaskChanged Done index (unsafeGetItem file index)
+                           (unsafeGetItem newTodoFile index)]
 
 
     describe "no items given" $ do
       prop "leaves file unchanged and performs no actions" $
-        \file day -> doItems day file [] == (file, [])
+        \file day -> doItems day file [] `shouldBe` (file, [])
 
 
 spec :: Spec
@@ -91,21 +91,21 @@ spec = describe "Core operations on todos" $ do
 
     describe "makePriority" $ do
       prop "converts lower-case" $
-        forAll (choose ('a', 'z')) $ \x -> makePriority x == makePriority (toUpper x)
+        forAll (choose ('a', 'z')) $ \x -> makePriority x `shouldBe` makePriority (toUpper x)
 
       prop "creates the same as unsafeMakePriority" $
-        forAll arbitraryPriorityLetter $ \x -> makePriority x == Just (unsafeMakePriority x)
+        forAll arbitraryPriorityLetter $ \x -> makePriority x `shouldBe` Just (unsafeMakePriority x)
 
       prop "returns Nothing for non-priorities" $
-        \x -> not (isAlpha x) ==> makePriority x == Nothing
+        \x -> not (isAlpha x) ==> makePriority x `shouldBe` Nothing
 
   describe "marking items as done" $ do
     prop "markAsDone implies isDone" $
       \date item -> isDone (markAsDone date item)
     prop "markAsDone sets date completed if not done" $
-      \date item -> not (isDone item) ==> dateCompleted (markAsDone item date) == Just date
+      \date item -> not (isDone item) ==> dateCompleted (markAsDone item date) `shouldBe` Just date
     prop "markAsDone leaves date completed if already done" $
-      \date item -> isDone item ==> dateCompleted (markAsDone item date) == (dateCompleted item)
+      \date item -> isDone item ==> dateCompleted (markAsDone item date) `shouldBe` (dateCompleted item)
     prop "markAsUndone implies not done" $
       not . isDone . markAsUndone
 
@@ -119,57 +119,57 @@ spec = describe "Core operations on todos" $ do
   describe "todo files" $ do
     describe "numItems" $ do
       prop "shows zero when there are no items" $
-        \x -> numItems (makeTodoFile x []) == 0
+        \x -> numItems (makeTodoFile x []) `shouldBe` 0
       prop "shows the number of items" $
-        \name items -> numItems (makeTodoFile name items) == length items
+        \name items -> numItems (makeTodoFile name items) `shouldBe` length items
     describe "isEmpty" $ do
       prop "is equivalent to numItems is 0" $
-        \x -> (numItems x == 0) == isEmpty x
+        \x -> (numItems x == 0) `shouldBe` isEmpty x
     describe "listItems" $ do
       prop "lists items with 1-based index" $
-        \name items -> listItems (makeTodoFile name items) == zip [1..] items
+        \name items -> listItems (makeTodoFile name items) `shouldBe` zip [1..] items
       prop "is consistent with numItems" $
-        \file -> length (listItems file) == numItems (file)
+        \file -> length (listItems file) `shouldBe` numItems (file)
     describe "filterItems" $ do
       prop "includes only items that satisfy a predicate" $
         \name items -> all (isDone . snd) $ filterItems isDone (makeTodoFile name items)
       prop "refers to items by their index in the original list" $
         \name items -> all (\(i, t) -> items !! (i - 1) == t) $ filterItems isDone (makeTodoFile name items)
       prop "is equivalent to listItems" $ do
-        \file -> filterItems (const True) file == listItems file
+        \file -> filterItems (const True) file `shouldBe` listItems file
     describe "allItems" $ do
       prop "is equivalent to listItems but without the indexes" $
-        \file -> allItems file == map snd (listItems file)
+        \file -> allItems file `shouldBe` map snd (listItems file)
     describe "getItem" $ do
       prop "returns Nothing when out of range" $
-        \name items i -> i < 1 || i > length items ==> getItem (makeTodoFile name items) i == Nothing
+        \name items i -> i < 1 || i > length items ==> getItem (makeTodoFile name items) i `shouldBe` Nothing
       prop "returns the item when in range (1-based index)" $
         \name -> forAll (listOf1 arbitrary) $ \items ->
         forAll (choose (1, length items)) $ \i ->
-        getItem (makeTodoFile name items) i == Just (items !! (i - 1))
+        getItem (makeTodoFile name items) i `shouldBe` Just (items !! (i - 1))
       prop "aligns with listItem" $
         forAll todoFileIndexes $ \(file, i) ->
-        [(i, fromJust (getItem file i))] == [(n, t) | (n, t) <- listItems file, i == n]
+        [(i, fromJust (getItem file i))] `shouldBe` [(n, t) | (n, t) <- listItems file, i == n]
     describe "unsafeGetItem" $ do
       prop "returns the item when in range (1-based index)" $
         forAll todoFileIndexes $ \(file, i) ->
-        getItem file i == Just (unsafeGetItem file i)
+        getItem file i `shouldBe` Just (unsafeGetItem file i)
     describe "replaceItem" $ do
       prop "swaps out the specified item" $
         \item -> forAll todoFileIndexes $ \(file, i) ->
         let oldList = listItems file
             newList = listItems (replaceItem file i item) in
-        newList == take (i - 1) oldList ++ [(i, item)] ++ drop i oldList
+        newList `shouldBe` take (i - 1) oldList ++ [(i, item)] ++ drop i oldList
       prop "does not alter the size of the todo file" $
         \item -> forAll todoFileIndexes $ \(file, i) ->
-        numItems file == numItems (replaceItem file i item)
+        numItems (replaceItem file i item) `shouldBe` numItems file
 
   describe "archive" $ do
     prop "strips all done items" $
       all (not . isDone) . allItems . fst . archive
     prop "does not remove any not done items" $
-      \file -> filter (not . isDone) (allItems file) == (allItems . fst . archive) file
+      \file -> (allItems . fst . archive) file `shouldBe` filter (not . isDone) (allItems file)
     prop "returns only done items" $
       all isDone . snd . archive
     prop "returns precisely the done items" $
-      \file -> filter isDone (allItems file) == snd (archive file)
+      \file -> snd (archive file) `shouldBe` filter isDone (allItems file)
