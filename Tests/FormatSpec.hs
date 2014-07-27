da06a92163bfae46eab2ad159fc6ba070601cf47
Jonathan Lange <jml@mumak.net>
2014-07-27 16:20:13 +0100

    Whitespace

--- Tests/FormatSpec.hs
+++ Tests/FormatSpec.hs
@@ -14,6 +14,7 @@ spec = describe "Formatting user output" $ do
   describe "appMessage" $ do
     prop "returns a string prefixed by the appName" $
       \x -> appMessage x == appName ++ ": " ++ x
+
   describe "format" $ do
     describe "priority" $ do
       prop "shows priorities as (N)" $
