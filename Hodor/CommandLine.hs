eb3a92532d25deeb923c7635eff419c97a4b8807
Jonathan Lange <jml@mumak.net>
2014-08-17 13:18:10 +0100

    Specify commands using a data structure

--- Hodor/CommandLine.hs
+++ Hodor/CommandLine.hs
@@ -3,6 +3,7 @@
 module Hodor.CommandLine where
 
 import Data.Maybe (fromMaybe)
+import Data.Monoid (mconcat)
 import Options.Applicative
 
 import Hodor.Commands
@@ -22,55 +23,63 @@ globalOptions = Options
       <$> optional (strOption (long "todo-file" <> metavar "FILE" <> help "location of todo file"))
       <*> optional (strOption (long "done-file" <> metavar "FILE" <> help "location of done file"))
       <*> optional (strOption (long "config-file" <> metavar "FILE" <> help "location of config file"))
-      <*> subparser (
-        command "add" add
-        <> command "a" add
-        <> command "append" append
-        <> command "app" append
-        <> command "archive" archive
-        <> command "depri" depri
-        <> command "dp" depri
-        <> command "do" doCmd
-        <> command "list" list
-        <> command "ls" list
-        <> command "listcon" lsc
-        <> command "lsc" lsc
-        <> command "listpri" lsp
-        <> command "lsp" lsp
-        <> command "listproj" lsprj
-        <> command "lsprj" lsprj
-        <> command "prepend" prepend
-        <> command "prep" prepend
-        <> command "pri" pri
-        <> command "p" pri
-        <> command "undo" undo
-        )
-      where
-        add     = info addOptions (progDesc "add todo item")
-        archive = info (pure ArchiveCommand) (progDesc "archive done items")
-        append  = info appendOptions (progDesc "append text to an item")
-        depri   = info depriOptions (progDesc "de-prioritize an item")
-        doCmd   = info doOptions (progDesc "mark item as done")
-        list    = info listOptions (progDesc "list todo items")
-        lsc     = info (pure ListContextCommand) (progDesc "list contexts")
-        lsp     = info (pure ListPriorityCommand) (progDesc "list items of high priority")
-        lsprj   = info (pure ListProjectCommand) (progDesc "list projects")
-        prepend = info prependOptions (progDesc "prepend text to an item")
-        pri     = info priOptions (progDesc "prioritize an item")
-        undo    = info undoOptions (progDesc "mark item as not done")
+      <*> subcommands hodorCommands
 
-        addOptions     = AddCommand <$> wordsOption
-        appendOptions  = AppendCommand <$> itemOption <*> wordsOption
-        depriOptions   = DeprioritizeCommand <$> itemOption
-        doOptions      = DoCommand <$> itemsOption
-        listOptions    = ListCommand <$> many (argument str (metavar "FILTERS..."))
-        prependOptions = PrependCommand <$> itemOption <*> wordsOption
-        priOptions     = PrioritizeCommand <$> itemOption <*> (argument str (metavar "PRIORITY"))
-        undoOptions    = UndoCommand <$> itemsOption
 
-        itemOption = argument auto (metavar "ITEM")
-        itemsOption = some itemOption
-        wordsOption = many (argument str (metavar "WORDS..."))
+subcommands :: [(String, ParserInfo a)] -> Parser a
+subcommands cmds = subparser $ mconcat $ map (uncurry command) cmds
+
+
+hodorCommands :: [(String, ParserInfo Command)]
+hodorCommands = [
+  ("add", add),
+  ("a", add),
+  ("append", append),
+  ("app", append),
+  ("archive", archive),
+  ("depri", depri),
+  ("dp", depri),
+  ("do", doCmd),
+  ("list", list),
+  ("ls", list),
+  ("listcon", lsc),
+  ("lsc", lsc),
+  ("listpri", lsp),
+  ("lsp", lsp),
+  ("listproj", lsprj),
+  ("lsprj", lsprj),
+  ("prepend", prepend),
+  ("prep", prepend),
+  ("pri", pri),
+  ("p", pri),
+  ("undo", undo)
+  ]
+  where
+    add     = info addOptions (progDesc "add todo item")
+    archive = info (pure ArchiveCommand) (progDesc "archive done items")
+    append  = info appendOptions (progDesc "append text to an item")
+    depri   = info depriOptions (progDesc "de-prioritize an item")
+    doCmd   = info doOptions (progDesc "mark item as done")
+    list    = info listOptions (progDesc "list todo items")
+    lsc     = info (pure ListContextCommand) (progDesc "list contexts")
+    lsp     = info (pure ListPriorityCommand) (progDesc "list items of high priority")
+    lsprj   = info (pure ListProjectCommand) (progDesc "list projects")
+    prepend = info prependOptions (progDesc "prepend text to an item")
+    pri     = info priOptions (progDesc "prioritize an item")
+    undo    = info undoOptions (progDesc "mark item as not done")
+
+    addOptions     = AddCommand <$> wordsOption
+    appendOptions  = AppendCommand <$> itemOption <*> wordsOption
+    depriOptions   = DeprioritizeCommand <$> itemOption
+    doOptions      = DoCommand <$> itemsOption
+    listOptions    = ListCommand <$> many (argument str (metavar "FILTERS..."))
+    prependOptions = PrependCommand <$> itemOption <*> wordsOption
+    priOptions     = PrioritizeCommand <$> itemOption <*> (argument str (metavar "PRIORITY"))
+    undoOptions    = UndoCommand <$> itemsOption
+
+    itemOption = argument auto (metavar "ITEM")
+    itemsOption = some itemOption
+    wordsOption = many (argument str (metavar "WORDS..."))
 
 
 -- XXX: Specific reader for priority (i.e. delete Commands.getPriority)
