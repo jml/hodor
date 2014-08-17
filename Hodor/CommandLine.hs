b34a49fa7cd4d66639cb20c4d7f01401da9f27d0
Jonathan Lange <jml@mumak.net>
2014-08-17 12:26:38 +0100

    Add aliases, make data more obvious

--- Hodor/CommandLine.hs
+++ Hodor/CommandLine.hs
@@ -23,20 +23,42 @@ globalOptions = Options
       <*> optional (strOption (long "done-file" <> metavar "FILE" <> help "location of done file"))
       <*> optional (strOption (long "config-file" <> metavar "FILE" <> help "location of config file"))
       <*> subparser (
-        command "list" (info listOptions (progDesc "list todo items"))
-        <> command "add" (info addOptions (progDesc "add todo item"))
-        <> command "do" (info doOptions (progDesc "mark item as done"))
-        <> command "undo" (info undoOptions (progDesc "mark item as not done"))
-        <> command "archive" (info (pure ArchiveCommand) (progDesc "archive done items"))
-        <> command "lsc" (info (pure ListContextCommand) (progDesc "list contexts"))
-        <> command "lsp" (info (pure ListPriorityCommand) (progDesc "list items of high priority"))
-        <> command "lsprj" (info (pure ListProjectCommand) (progDesc "list projects"))
-        <> command "pri" (info priOptions (progDesc "prioritize an item"))
-        <> command "depri" (info depriOptions (progDesc "de-prioritize an item"))
-        <> command "app" (info appendOptions (progDesc "append text to an item"))
-        <> command "pre" (info prependOptions (progDesc "prepend text to an item")))
+        command "add" add
+        <> command "a" add
+        <> command "append" append
+        <> command "app" append
+        <> command "archive" archive
+        <> command "depri" depri
+        <> command "dp" depri
+        <> command "do" doCmd
+        <> command "list" list
+        <> command "ls" list
+        <> command "listcon" lsc
+        <> command "lsc" lsc
+        <> command "listpri" lsp
+        <> command "lsp" lsp
+        <> command "listproj" lsprj
+        <> command "lsprj" lsprj
+        <> command "prepend" prepend
+        <> command "prep" prepend
+        <> command "pri" pri
+        <> command "p" pri
+        <> command "undo" undo
+        )
+      where
+        add     = (info addOptions (progDesc "add todo item"))
+        archive = (info (pure ArchiveCommand) (progDesc "archive done items"))
+        append  = (info appendOptions (progDesc "append text to an item"))
+        depri   = (info depriOptions (progDesc "de-prioritize an item"))
+        doCmd   = (info doOptions (progDesc "mark item as done"))
+        list    = (info listOptions (progDesc "list todo items"))
+        lsc     = (info (pure ListContextCommand) (progDesc "list contexts"))
+        lsp     = (info (pure ListPriorityCommand) (progDesc "list items of high priority"))
+        lsprj   = (info (pure ListProjectCommand) (progDesc "list projects"))
+        prepend = (info prependOptions (progDesc "prepend text to an item"))
+        pri     = (info priOptions (progDesc "prioritize an item"))
+        undo    = (info undoOptions (progDesc "mark item as not done"))
 
--- XXX: Add aliases
 -- XXX: Maybe move stuff into 'where' of globalOptions
 -- XXX: Specific reader for priority (i.e. delete Commands.getPriority)
 
